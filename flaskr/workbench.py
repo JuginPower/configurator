2f52da99bd3775360cf74cea856f861a8c83a7b7
JuginPower <juginpower90@gmail.com>
2023-06-08 11:48:17 +0200

    feature-monolit: add crawling scripts

--- flaskr/workbench.py
+++ flaskr/workbench.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import abort
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
-import requests
+from model import Price, Indiz
 
 
 work = Blueprint('workbench', __name__)
@@ -24,16 +24,18 @@ def monitor():
     if request.method == 'POST':
         id = request.form['id']
         amount = request.form['amount']
-        json_data = requests.get(urlstring+"/price/"+str(id)+"/"+str(amount)).json()
+        price_object = Price(id)
+        dates = price_object.get_dates(amount)
+        prices = price_object.get_closes(amount)
         response_html = "<tr><th>Datum</th><th>Uhrzeit</th><th>Preis</th></tr>"
-        for date, price in zip(json_data["Datum"], json_data["Preis"]):
+        for date, price in zip(dates, prices):
             dateobj = datetime.strptime(date, "%a, %d %b %Y %H:%M:%S %Z")
             row = "<tr><td>" + dateobj.strftime("%Y-%m-%d") + "</td><td>"+ dateobj.strftime("%H:%M:%S") + "</td><td>" + str(price) + "</td></tr>"
             response_html += row
         return response_html
     
-    indizdata = {"ids": get_id(), "names": get_name()}
-    return render_template('workbench/monitor.html', indizdata=indizdata)
+    indizData = get_indiz_data()
+    return render_template('workbench/monitor.html', indiz_data=indizData)
 
 
 @work.route('/indicator')
@@ -41,29 +43,20 @@ def indicator():
     if request.method == 'POST':
         id = request.form['id']
 
-    indizdata = {"ids": get_id(), "names": get_name()}
-    return render_template('workbench/indicator.html', indizdata=indizdata)
+    indizData = get_indiz_data()
+    return render_template('workbench/indicator.html', indiz_data=indizData)
 
 
-def get_id(name=None):
+def get_indiz_data(id=None, name=None):
+
+    data = {}
+    indiz_object = Indiz()
     
+    if id:
+        data.update({"name": indiz_object.get_one_name(id)})
     if name:
-        return requests.get(urlstring + f"/id/{name}").json()
-    else:
-        return requests.get(urlstring + f"/id").json()
+        data.update({"id": indiz_object.get_one_id(name)})
+    if not id and not name:
+        data.update({"ids": indiz_object.get_ids(), "names": indiz_object.get_names()})
 
-
-def get_name(id=None):
-
-    if id:
-        return requests.get(urlstring + f"/name/{id}").json()
-    else:
-        return requests.get(urlstring + f"/name").json()
-
-
-def get_indiz(id=None):
-
-    if id:
-        return requests.get(urlstring + f"/indiz/{id}").json()
-    else:
-        return requests.get(urlstring + "/indiz").json()
+    return data
---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+from model.indiz import *
+from model.price import *
+from model.urlmanager import *
+from model.crawler_float import *
+from model.datalayer_mysql_main import *
